5da53a74c6a6a1f80ac14f8281af377fd1826873
ToAHuG <c121776235@gmail.com>
2024-10-31 00:13:04 +0800

    修改Car程式
    依照滑鼠的移動改變
    車子移動的距離

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 public class Car : MonoBehaviour
 {
     float speed = 0;
+    Vector2 startPos;
     void Start()
     {
     }
     void Update()
     {
+        //(新增)取得滑動長度
         if (Input.GetMouseButtonDown(0))
-        { //點擊滑鼠時
-            speed = 0.2f; //設定初始速度
+        {
+            startPos = Input.mousePosition; //將點擊滑鼠時的座標記錄起來
         }
-        transform.Translate(speed, 0, 0); //移動
-        speed *= 0.98f; //減速
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition; //將放開滑鼠時的座標記錄起來
+            float swipeLength = endPos.x - startPos.x;
+            speed = swipeLength / 500.0f; //把滑動長度轉換成初始移動速度
+        }
+        transform.Translate(speed, 0, 0);
+        speed *= 0.98f;
     }
 }
